e0bad78a4bebdc817c669ffccdfaf73363e33e1d
Elambharathi Thangavel <elambharathi2002.pt@gmail.com>
2026-04-14 09:33:13 +0530

    KAN-4: Change the header text from Learn React to Hello Phase 5

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello Phase 5 link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello Phase 5/i);
   expect(linkElement).toBeInTheDocument();
 });
