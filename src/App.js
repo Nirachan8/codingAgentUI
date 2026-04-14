3964fdd13d4ba862c5de2e1347731bfc34ee0477
Elambharathi Thangavel <elambharathi2002.pt@gmail.com>
2026-04-14 11:38:08 +0530

    KAN-4: Change the header link text from Learn React to Phase 6 Ready

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Phase 6 Ready
         </a>
       </header>
     </div>
