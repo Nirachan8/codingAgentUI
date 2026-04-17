dab5e11ed9a0d6c19a99694dc4b648e9d7e37d4c
Elambharathi Thangavel <elambharathi2002.pt@gmail.com>
2026-04-17 09:30:01 +0530

    KAN-11: Change the link text from Learn React to Screenshots Work

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Screenshots Work
         </a>
       </header>
     </div>
